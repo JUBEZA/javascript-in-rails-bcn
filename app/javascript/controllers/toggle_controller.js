5f84595ce1928148499ef40847e0383a981d2eef
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-02-23 15:41:29 -0300

    Toggle Controller listo

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,6 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  connect() {
+  static targets = ["togglableElement"]
+
+  fire() {
+    this.togglableElementTarget.classList.toggle("d-none");
   }
 }
